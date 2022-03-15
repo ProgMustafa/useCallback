b9cdc0e4ef164e9ae3a1cc449c6f68385dcc9d84
Prog Mustafa <prog.mustafa98@gmail.com>
2022-03-15 12:46:16 +0200

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+// without **useCallBack**
 
-function App() {
+// import React, { useState} from 'react';
+// const funccount = new Set();
+// const App = () => {
+
+//   const [count, setCount] = useState(0)
+//   const [number, setNumber] = useState(0)
+
+//   const incrementCounter = () => {
+//     setCount(count + 1)
+//   }
+//   const decrementCounter = () => {
+//     setCount(count - 1)
+//   }
+
+//    const incrementNumber = () => {
+//     setNumber(number + 1)
+//   }
+
+// funccount.add(incrementCounter);
+// funccount.add(decrementCounter);
+// funccount.add(incrementNumber);
+// alert(funccount.size);
+
+//   return (
+//     <div>
+//       Count: {count}
+//       <button onClick={incrementCounter}>
+//         Increase counter
+//       </button>
+//       <button onClick={decrementCounter}>
+//          Decrease Counter
+//       </button>
+//       <button onClick={incrementNumber}>
+//         increase number
+//       </button>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+// with useCallBack
+
+import React, { useState, useCallback } from "react";
+import './App.css';
+import { Button } from "antd";
+import 'antd/dist/antd.css';
+var funccount = new Set();
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [number, setNumber] = useState(0);
+
+  const incrementCounter = useCallback(() => {
+    setCount(count + 1);
+  }, [count]);
+  const decrementCounter = useCallback(() => {
+    setCount(count - 1);
+  }, [count]);
+  const incrementNumber = useCallback(() => {
+    setNumber(number + 1);
+  }, [number]);
+
+  funccount.add(incrementCounter);
+  funccount.add(decrementCounter);
+  funccount.add(incrementNumber);
+  alert(funccount.size);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Count: {count}
+      <Button  onClick={incrementCounter}>Increase counter</Button>
+      <Button  onClick={decrementCounter}>Decrease Counter</Button>
+      <Button  onClick={incrementNumber}>increase number</Button>
     </div>
   );
-}
+};
 
 export default App;
